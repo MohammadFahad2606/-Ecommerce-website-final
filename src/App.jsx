aab0d62118728f202264378b0fb3fb5959f7f268
MohammadFahad2606 <design4youonline@gmail.com>
2024-11-18 13:47:41 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Home from './Pages/Home'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
-const App = () => {
-  return (
-    <div>
-      <Home/>
-          </div>
-  )
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
